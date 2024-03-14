757bde826ec2679262fd1e2b5ca95d3554fcba94
Kauan Machado <kauanmachad0@outlook.com>
2024-03-13 21:16:48 -0300

    first commit

---
+++ CloudStorageTest.API/Controllers/StorageController.cs
@@ -0,0 +1,18 @@
+﻿using CloudStorageTest.Application.UseCases.Users.UploadProfilePhoto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudStorageTest.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class StorageController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult UploadImage(IFormFile file)
+    {
+        var useCase = new UploadProfilePhotoUseCase();
+        useCase.Execute(file);
+
+        return Created();
+    }
+}
---
+++ CloudStorageTest.Application/UseCases/Users/UploadProfilePhoto/UploadProfilePhotoUseCase.cs
@@ -0,0 +1,37 @@
+﻿using CloudStorageTest.Domain.Entities;
+using CloudStorageTest.Domain.Storage;
+using FileTypeChecker.Extensions;
+using FileTypeChecker.Types;
+using Microsoft.AspNetCore.Http;
+
+namespace CloudStorageTest.Application.UseCases.Users.UploadProfilePhoto;
+public class UploadProfilePhotoUseCase
+{
+    private readonly IStorageService _storageService;
+    public UploadProfilePhotoUseCase(IStorageService storageService)
+    {
+        _storageService = storageService;
+    }
+    public void Execute(IFormFile file)
+    {
+        var fileStream = file.OpenReadStream();
+
+        var isImage = fileStream.Is<JointPhotographicExpertsGroup>();
+
+        if (isImage == false)
+            throw new Exception("This file is not an image");
+
+        var user = GetFromDatabase();
+
+        _storageService.Upload(file, user);
+    }
+    private User GetFromDatabase()
+    {
+        return new User
+        {
+            Id = 1,
+            Name = "Kauan",
+            Email = "kauan@gmail.com"
+        };
+    }
+}
